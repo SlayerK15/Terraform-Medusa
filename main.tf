62db2e10d6aac0ce75f947090bee48bc5d13f595
Kanav <gathekanav@gmail.com>
2024-09-05 19:05:17 +0530

    Initail commit

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa" {
+  family                   = "medusa-service"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "medusa"
+      image     = "medusajs/medusa-server"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 9000
+          hostPort      = 9000
+        }
+      ]
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = "postgres://username:password@hostname:port/dbname"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-06e061d9e41f360d6", "subnet-08afa8e9e0d30af6b"]
+    security_groups  = ["sg-08c3cd7d15aa177e3"]
+    assign_public_ip = true
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "aws_account_id" {
+  default = "897722690232"  
+}
